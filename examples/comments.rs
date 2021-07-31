0270e31de165718f86efe97ea9f72994fd7a4eed
Mateus Nunes <mateusnss@protonmail.ch>
2021-07-31 13:48:15 -0300

    added comments

---
+++ examples/comments.rs
@@ -0,0 +1,11 @@
+
+#[allow(unused_mut)]
+#[allow(unused_variables)]
+
+/// This is documentation.
+/// This too.
+fn main(){
+
+    let mut /* This is a comment between the code*/ a:u8 = 23u8; let b:u32 = 434_542_653u32; // Comment.
+    // Comment.
+}
